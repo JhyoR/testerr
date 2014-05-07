91b4498c6c110aaade3cd7169e91c4b4646f9ef7
JhyoR <jhyoramona@gmail.com>
2014-05-08 00:31:43 +0700

    finish try

--- js/index.js
+++ js/index.js
@@ -2,20 +2,25 @@
   var $hoverlayer = $('.js-hoverlayer');
   var $thumbin = $('.js-thumbin');
   var $reupload = $('.js-reupload');
+  var $sharemodal = $('.js-sharemodal');
+  var $regtrigger = $('.js-regtrigger');
+  var $registermodal = $('.js-registermodal');
   var $sectionshare = $('.js-sectionshare');
   var $userfile = $('.js-userfile');
   var $commentfile = $('.js-commentfile');
   var $sectionupload = $('.js-sectionupload');
   var $uploadbox = $('.js-uploadbox');
   var $sectionhome = $('.js-sectionhome');
-  var $panqtrigger = $('.js-panqtrigger');
-  var $reuploadtrigger = $('.js-reuploadtrigger');
-  var $sharetrigger = $('.js-sharetrigger');
-  var $questionpan = $('.js-questionpan');
   var $loginmodal = $('.js-loginmodal');
   var $username = $('.js-username');
   var $progressindicator = $('.js-progressindicator');
   var $logomain = $('.js-logomain');
+  var $mainheader = $('.js-mainheader');
+  var $uniqform = $('.js-uniqform');
+  var $panqtrigger = $('.js-panqtrigger');
+  var $reuploadtrigger = $('.js-reuploadtrigger');
+  var $sharetrigger = $('.js-sharetrigger');
+  var $questionpan = $('.js-questionpan');
 
 
   function homeSection(e) {
@@ -31,19 +36,21 @@
   function uploadSection(e) {
     $sectionupload.show();
     $progressindicator.show().find('div').css('background-color' , 'rgb(118, 238, 111)').not('div:nth-child(1)').css('background-color' , 'rgba(0, 0, 0, 0)');
-    $questionpan.show();
+    $questionpan.show().find('.uniqform p').text('Question for this file');;
   	$sectionhome.hide();
     $reupload.hide();
     $sectionshare.hide();
+    $mainheader.attr('style', 'height: 50px');
   }
   function reuploadSection(e) {
   	$reupload.show();
     $progressindicator.show().find('div').css('background-color' , 'rgb(118, 238, 111)').not('div:nth-child(1)').css('background-color' , 'rgba(0, 0, 0, 0)');
-    $questionpan.show();
+    $questionpan.show().find('.uniqform p').text('General question');
     $sectionhome.hide(); 
     $sectionupload.hide();
     $hoverlayer.hide();
     $sectionshare.hide();
+    $mainheader.attr('style', 'height: 50px');
   }
   function shareSection(e) {
     $sectionshare.show();
@@ -54,6 +61,8 @@
     $reupload.hide();
     $loginmodal.hide();
     $questionpan.hide();
+    $registermodal.hide();
+    $mainheader.attr('style', 'height: 50px');
   }
 
   //----homepage-----//
@@ -89,9 +98,11 @@
     if ($panqtrigger.hasClass('uped')){
   		$questionpan.attr('style', 'bottom: -400px');
     	$(this).removeClass('uped');
+      $mainheader.attr('style', 'height: 50px');
     } else {
     	$questionpan.css('bottom' , '0');
       $(this).addClass('uped');
+      $mainheader.attr('style', 'height: 100%');
     }
   });
   $reuploadtrigger.click(function(){
@@ -115,5 +126,14 @@
   //-----share-----//
   $sharetrigger.click(function(){
   	shareSection();
+    
+  });
+  $regtrigger.click(function(){
+  	$registermodal.show();
+    $sharemodal.hide();
+    $(this).addClass('feedtrigger').attr('style' , '500');
+  });
+  $('.feedtrigger').click(function(){
+  	
   });
 })();
