f021d45d298c51af6cb1fe309d3bcdff6225cdbb
JhyoR <jhyoramona@gmail.com>
2014-05-14 03:53:32 +0700

    fghf

--- js/fie-registered.js
+++ js/fie-registered.js
@@ -2,21 +2,21 @@ $(document).ready(function () {
 
 $(".qpanel").not(".submit, .submitted").addClass("submit").find(".qopen").show().siblings().hide();
 
-$(".bg").click(function(e){
+$(".bg").on("click", function(e){
 	var x = e.pageX,
-		y = e.pageY;
-	$(".qpinopen").css({top: y - '120', left: x - '190'});
+		y = e.pageY;	
+	$(".qpinopen").not(".print").css({top: y - '120', left: x - '190'});
 });
 
 $(".qpanel").on("click", function() {
 	if ($(this).is(".submit")) {
 		$(this).find(".qopen").hide().siblings().show();
 		$(this).removeClass("submit").addClass("submitted");
-	} else if ($(this).is(".submitted")) {
-		$(this).find(".qclose").hide().siblings().show();
-		$(this).removeClass("submitted").addClass("submit");
+		$(this).parents(".qpinopen").addClass("print").clone().addClass("new").css("left" , "-100%").removeClass("print").insertAfter(".qpinopen")
+		.find(".submitted").removeClass("submitted").addClass("submit").find(".qopen").show().siblings().hide();
 	}
 });
+$(".new").siblings().find(".qpanel").hide();
 
 
 
