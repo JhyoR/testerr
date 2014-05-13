4f57643ed4fa7f23274929e847b79e26a65cd576
JhyoR <jhyoramona@gmail.com>
2014-05-13 20:49:54 +0700

    active

---
+++ js/home-registered.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+$(".profile")
+	.mouseenter(function() {
+		$(".mainnav").show("500");
+		$(".mainnav")
+			.mouseenter(function(){
+				$(this).show();
+			})
+			.mouseleave(function(){
+				$(this).hide();
+			});
+	})
+	.mouseleave(function() {
+		$(".mainnav").hide();
+	});
+
+
+
+
+
+
+
+});
