a5688da2a1ab41dc21e50ce2c75acf00158806fc
JhyoR <jhyoramona@gmail.com>
2014-05-14 07:59:44 +0700

    qew

---
+++ js/filefolder.js
@@ -0,0 +1,14 @@
+$(".profile")
+	.mouseenter(function() {
+		$(".mainnav").show("500");
+		$(".mainnav")
+			.mouseenter(function(){
+				$(this).show();
+			})
+			.mouseleave(function(){
+				$(this).hide();
+			});
+	})
+	.mouseleave(function() {
+		$(".mainnav").hide();
+	});
