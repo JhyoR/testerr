c94ffad5e8e33c4d959fcb765c9d631a810f66ae
JhyoR <jhyoramona@gmail.com>
2014-05-14 07:36:29 +0700

    sadad

---
+++ js/project-registered.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+$(".question-section").on("click", function(){
+	if ($(this).is(".expand")) {
+		$(this).removeClass("expand").css("bottom" , "-340px");
+	} else {
+		$(this).addClass("expand").css("bottom" , "0px");
+	}
+});
+$(".profile")
+	.mouseenter(function() {
+		$("nav").show("500");
+		$("nav")
+			.mouseenter(function(){
+				$(this).show();
+			})
+			.mouseleave(function(){
+				$(this).hide();
+			});
+	})
+	.mouseleave(function() {
+		$("nav").hide();
+	});
+
+
+});
